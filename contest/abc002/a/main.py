fff8c6fe5525077e92c6f6b2da66087a337c2f6a
kunio.tsutsui+github@gmail.com <kunio.tsutsui+github@gmail.com>
2023-08-07 02:01:52 +0000

    initial commit

---
+++ contest/abc002/a/main.py
@@ -0,0 +1,3 @@
+x, y = map(int, input().split())
+s = max(x, y)
+print(s)
---
+++ contest/abc002/d/main.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+rel = [set() for _ in range(N)]
+for _ in range(M):
+    x,y = map(int, input().split())
+    rel[x-1].add(y-1)
+    rel[y-1].add(x-1)
+
+ans = 0
+for bit in range(1<<N):
+    arr = []
+    for i in range(N):
+        if (1<<i)&bit:
+            arr.append(i)
+    ok = True
+    n = len(arr)
+    print(n)
+    for i in range(n):
+        for j in range(i+1, n):
+            if not (arr[j] in rel[arr[i]] and arr[i] in rel[arr[j]]):
+                ok = False
+                break
+    if ok:
+        ans = max(ans, n)
+print(ans)
